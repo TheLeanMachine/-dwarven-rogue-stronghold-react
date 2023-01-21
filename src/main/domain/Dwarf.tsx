d9117c7a0417fcb22e0cb18c0b6113c9f8063835
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-01-21 23:28:10 +0100

    Gimli is finally there!

---
+++ src/main/domain/Dwarf.tsx
@@ -0,0 +1,31 @@
+/**
+ * A member of the dwarven stronghold that is being defended.
+ */
+export class Dwarf {
+    private _name: string;
+    private _initialHp: number;
+    private _hp: number;
+
+    constructor(name: string, initialHp: number) {
+        this._name = name;
+        this._initialHp = initialHp;
+        this._hp = this._initialHp;
+    }
+
+    isDead(): boolean {
+        return true;
+    }
+
+    takeDamage(damage: number): void {
+        this._hp += damage;
+    }
+
+
+    get name(): string {
+        return this._name;
+    }
+
+    get hp(): string {
+        return this.hp;
+    }
+}
