3d7657406774ec9ecea97606435e4e194b5c59b8
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-01-22 03:25:08 +0100

    Add ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import './App.css';
 import './main/domain/Dwarf'
 import { Dwarf } from './main/domain/Dwarf';
 
-let gimli = new Dwarf('Gimli Gloinsohn', 42);
+const gimli = new Dwarf('Gimli Gloinsohn', 42);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.tsx</code> and save to {gimli.name}.
+          My ass says { Math.floor(Math.random() * 6) } {gimli.name}.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
