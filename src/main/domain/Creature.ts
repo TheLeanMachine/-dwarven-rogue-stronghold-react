2c1a56913553225d0b49093f2ae1922e67b6b374
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-01-29 20:28:53 +0100

    Extract super-class

---
+++ src/main/domain/Creature.ts
@@ -0,0 +1,24 @@
+/**
+ * Anything that moves around the map and can be killed (uncluding undead ;-).
+ */
+export abstract class Creature {
+    private _initialHp: number;
+    private _hp: number;
+
+    constructor(initialHp: number) {
+        this._initialHp = initialHp;
+        this._hp = this._initialHp;
+    }
+
+    isDead(): boolean {
+        return this._hp <= 0;
+    }
+
+    takeDamage(damage: number): void {
+        this._hp += damage;
+    }
+
+    get hp(): number {
+        return this._hp;
+    }
+}
--- src/main/domain/Dwarf.ts
+++ src/main/domain/Dwarf.ts
@@ -1,30 +1,17 @@
+import { Creature } from './Creature';
+
 /**
  * A member of the dwarven stronghold that is being defended.
  */
-export class Dwarf {
+export class Dwarf extends Creature {
     private _name: string;
-    private _initialHp: number;
-    private _hp: number;
 
     constructor(name: string, initialHp: number) {
+        super(initialHp);
         this._name = name;
-        this._initialHp = initialHp;
-        this._hp = this._initialHp;
-    }
-
-    isDead(): boolean {
-        return this._hp <= 0;
-    }
-
-    takeDamage(damage: number): void {
-        this._hp += damage;
     }
 
     get name(): string {
         return this._name;
-    }
-
-    get hp(): number {
-        return this._hp;
     }
 }
